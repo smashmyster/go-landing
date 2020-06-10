a971aef3d5cd6e292199ff4eb0739dc3bb08022a
mahlekenyane <mahlekenyane@avospace.net>
2020-06-10 14:48:24 +0200

    routes

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,7 +1,7 @@
 export const IMAGE_API_ROOT = `https://khula.co.za/ProducePic/`
-export const BUYER_ROOT = "http://buyer.motoglo.co.za"
-export const FARMER_ROOT = "http://farmer.motoglo.co.za"
-export const GRAPH_API = "http://graph.motoglo.co.za/graphql"
+export const BUYER_ROOT = "https://buyer.khula.co.za"
+export const FARMER_ROOT = "https://farmer.khula.co.za"
+export const GRAPH_API = "https://graph.khula.co.za/graphql"
 export const _GetImagePath = (name: string) => name ? `${IMAGE_API_ROOT}${name.replace(/ /g, "").toLowerCase()}.png` : "";
 export const isMobile = window.innerWidth < 768
 
