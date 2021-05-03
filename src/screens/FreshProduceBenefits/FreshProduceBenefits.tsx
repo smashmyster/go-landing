9b970828c16645eee817e51d2d6406f5ad92adb0
MahlekenyaneTseole <mahtseole@gmail.com>
2021-05-03 14:00:11 +0200

    latest

--- src/screens/FreshProduceBenefits/FreshProduceBenefits.tsx
+++ src/screens/FreshProduceBenefits/FreshProduceBenefits.tsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const FreshProduceBenefits = () => {
   return (
-    <div className="screen fresh-produce-benefits-screen k-row k-row-center-items green-alpha-bg">
-      {'"'}Using the Khula Fresh Produce Trader means{'"'} section
+    <div className="screen fresh-produce-benefits-screen green-alpha-bg">
+      <div className="f-12 main-subtitle-text center-text mb-5">Khula app</div>
+      <div className="f-12 main-title-text center-text">
+        Using the KHULA Fresh Produce
+        <br />
+        Trader means
+      </div>
+      <div className="k-row k-row-center-items">
+        <div className="detail-container column-center">
+          <div className="icon-container k-row k-row-center-items">
+            <span className="icon fal-seedling" />
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/screens/ProductDetails/ProductDetails.tsx
+++ src/screens/ProductDetails/ProductDetails.tsx
@@ -1,35 +1,46 @@
 import React from "react";
 const inputsApp = require("assets/images/inputs-app.png");
 
-
 const ProductDetails = () => {
   return (
-    <div className="screen products-screen k-row k-row-center-items green-bg" style={{minHeight:500}}>
-      <div className="left-section">
-        <div className="section-name uppercase"> Our products</div>
-        <div className="header-txt h2">Are you a</div>
-        <div className="header-txt h1">Farmer?</div>
-        <p className="left-section-txt">Toggle our products on the right we offer an inputs app to buy over 2000 products, and a Fresh Produce Trader app to market your produce.</p>
+    <div className="screen products-screen col" style={{ minHeight: 500 }}>
+      <div className="k-row k-row-center-items details-container green-bg">
+        <div className="left-section section">
+          <div className="section-name uppercase"> Our products</div>
+          <div className="header-txt h2">Are you a</div>
+          <div className="header-txt h1">Farmer?</div>
+          <p className="left-section-txt">
+            Toggle our products on the right we offer an inputs app to buy over
+            2000 products, and a Fresh Produce Trader app to market your
+            produce.
+          </p>
+        </div>
+        <div className="input-app-container column-center">
+          <img className="input-app" src={inputsApp} alt="inputs market app" />
+        </div>
+        <div className="right-section section">
+          <div className="arrow-keys row">
+            <span className="icon fas-caret-left"></span>
+            <span className="icon fas-caret-right"></span>
+            <span className="key-label">Click Me!</span>
+          </div>
+          <div className="row section-headers">
+            <div style={{ color: "white" }}>Inputs Market</div>
+            <div style={{ color: "#85be87" }}>Fresh Produce Trader</div>
+          </div>
+          <p className="right-section-txt">
+            Buy your inputs the smart way. More intelligence, better prices,
+            from many suppliers at your fingertips.
+          </p>
+          <div className="row action-btns">
+            <div className="uppercase white-btn">Contact us</div>
+            <div className="uppercase transparent-btn">
+              Read More <span className="fal-long-arrow-right"></span>
+            </div>
+          </div>
+        </div>
       </div>
-      <img className="input-app" src={inputsApp} alt="inputs market app"/>
-      <div className="right-section">
-        <div className="arrow-keys row">
-          <span className="icon fas-caret-left"></span>
-          <span className="icon fas-caret-right"></span>
-          <span className="key-label">Click Me!</span>
-        </div>
-        <div className="row section-headers">
-          <div style={{color:"white"}}>Inputs Market</div>
-          <div style={{color:"#85be87"}}>Fresh Produce Trader</div>
-        </div>
-        <p className="right-section-txt">Buy your inputs the smart way. More intelligence, better prices, from many suppliers at your fingertips.</p>
-        <div className="row action-btns">
-          <div className="uppercase white-btn">Contact us</div>
-          <div className="uppercase transparent-btn">Read More <span className="fal-long-arrow-right"></span></div>
-        </div>
-      </div>
-      
-
+      <div className="section-color"></div>
     </div>
   );
 };
