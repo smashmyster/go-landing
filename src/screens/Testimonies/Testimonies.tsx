e1f2bafc0f65f8fb66d2ac17cbfdcb395bc42d5e
nmabaso <nontokozom@khula.co.za>
2021-05-03 14:40:32 +0200

    WIP

--- src/screens/Testimonies/Testimonies.tsx
+++ src/screens/Testimonies/Testimonies.tsx
@@ -3,7 +3,15 @@ import React from "react";
 const Testimonies = () => {
   return (
     <div className="screen testimonies-screen k-row k-row-center-items">
-      Testimonies section
+      <div className="uppercase sub-title">Don't just take our word for it.</div>
+      <div className="title">What our Buyers, Suppliers and Partners have to say.</div>
+      <div className="row">
+        <div className="left-section">
+          <div className="testimony">"I give the spraying program for cucumbers a thumbs up. The crop advisor 
+          (Johan Smith) looked after us, he was hands on and showed a lot of "</div>
+        </div>
+        <div className="right-section"></div>
+      </div>
     </div>
   );
 };
