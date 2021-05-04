81e0a51aa56d8e9136cd650ac0277d03ce536d1f
MahlekenyaneTseole <mahtseole@gmail.com>
2021-05-04 02:03:22 +0200

    landing

--- src/screens/Landing/Landing.tsx
+++ src/screens/Landing/Landing.tsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 const logo = require("assets/images/khula-logo.png");
 const truck = require("assets/images/truck.png");
 
 const Landing = () => {
+  const [shouldAnimate, setAnimate] = useState<boolean>(false);
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
   return (
     <div className="landing-screen k-row">
-      <div className="row">
-        <div className="left-section">
-          <img src={logo} alt="logo" />
-          <div className="company-name">Grow (Verb): Zulu</div>
-          <div className="company-description">
-            Tools and platforms to support and grow your business in the
-            agriculture supply chain
-          </div>
+      <div className="left-section">
+        <img src={logo} alt="logo" />
+        <div className="company-name">Grow (Verb): Zulu</div>
+        <div className="company-description">
+          Tools and platforms to support and grow your business in the
+          agriculture supply chain
         </div>
-        <div className="right-section">
-          <img src={truck} alt="truck" />
-        </div>
+      </div>
+      <div className="right-section">
+        <img
+          src={truck}
+          alt="truck"
+          className={`truck${shouldAnimate ? " animate" : ""}`}
+        />
       </div>
     </div>
   );
