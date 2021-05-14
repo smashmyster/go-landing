46b7aa3fe704fd80dae4a2bece1cd6e0895a9a14
nmabaso <nontokozom@khula.co.za>
2021-05-14 11:09:45 +0200

    WIP: mobile design for products market

--- src/screens/ProduceMarket/ProduceMarket.tsx
+++ src/screens/ProduceMarket/ProduceMarket.tsx
@@ -1,7 +1,9 @@
 import { isMobile } from "constants/index";
 import React from "react";
 const web = require("assets/images/web.png");
+const webMobile = require("assets/images/web-mobile.png");
 const app = require("assets/images/trader-app.png");
+const appMobile = require("assets/images/trader-app-mobile.png");
 
 const ProduceMarket = () => {
   return (
@@ -28,43 +30,67 @@ const ProduceMarket = () => {
             with our Spot Market, manage direct orders or program trading.
             Flexible terms and safe payment options.
           </div>
-          <div className="row action-btns">
-            <div className="uppercase green-btn pointer intercom-ic-launch">
-              Contact us
+          {!isMobile && (
+            <div className="row action-btns">
+              <div className="uppercase green-btn pointer intercom-ic-launch">
+                Contact us
+              </div>
+              {/* TODO: Create a page with more in-depth detail on the BW
+              <div className="transparent-btn pointer">
+                Find out more <span className="fal-long-arrow-right"></span>
+              </div> */}
             </div>
-            {/* TODO: Create a page with more in-depth detail on the BW
-            <div className="transparent-btn pointer">
-              Find out more <span className="fal-long-arrow-right"></span>
-            </div> */}
-          </div>
+          )}
         </div>
         <div className="right-section">
-          <img src={web} alt="web platform" className="web" />
+          <img
+            src={isMobile ? webMobile : web}
+            alt="web platform"
+            className="web"
+          />
         </div>
       </div>
-      <div className="row" style={{ alignItems: "center" }}>
-        <div className="left-section" style={{ width: "80%" }}>
-          <img src={app} alt="app platform" style={{ width: 500 }} />
+      <div
+        className="row"
+        style={{ alignItems: "center", marginTop: isMobile ? "90px" : "unset" }}
+      >
+        <div
+          className="left-section"
+          style={{ width: "80%", marginLeft: isMobile ? 0 : "" }}
+        >
+          <img
+            src={isMobile ? appMobile : app}
+            alt="app platform"
+            style={{ width: isMobile ? 200 : 500 }}
+          />
         </div>
-        <div className="right-section">
+        <div
+          className="right-section"
+          style={{ marginLeft: isMobile ? 20 : "" }}
+        >
           <div className="sub-title uppercase">In Beta</div>
           <div className="title">
             {`${isMobile ? "App" : " Fresh Produce Trader App"}`}
           </div>
-          <div className="section-text">
+          <div
+            className="section-text"
+            style={{ width: isMobile ? "80%" : "" }}
+          >
             Access the market on the go with our frsh produce trader app. Browse
             farmer availability and chat with your{" "}
             <span className="uppercase">Khula</span> suppliers on the go.
           </div>
-          <div className="row action-btns">
-            <div className="uppercase green-btn pointer intercom-ic-launch">
-              Contact us
+          {!isMobile && (
+            <div className="row action-btns">
+              <div className="uppercase green-btn pointer intercom-ic-launch">
+                Contact us
+              </div>
+              {/* TODO: Create a page with more in-depth detail on the TA
+              <div className="transparent-btn pointer">
+                Find out more <span className="fal-long-arrow-right"></span>
+              </div> */}
             </div>
-            {/* TODO: Create a page with more in-depth detail on the TA
-            <div className="transparent-btn pointer">
-              Find out more <span className="fal-long-arrow-right"></span>
-            </div> */}
-          </div>
+          )}
         </div>
       </div>
     </div>
