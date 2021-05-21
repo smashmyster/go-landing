65b358ea6fb7d53d733c5b585a68eced10e0befd
MahlekenyaneTseole <mahtseole@gmail.com>
2021-05-21 10:20:43 +0200

    logo loader

--- src/components/AppLoading/AppLoading.tsx
+++ src/components/AppLoading/AppLoading.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+const logo = require("assets/images/khula-logo-badge-green.svg");
 
 const AppLoading = () => {
   return (
     <div className="loading-screen roboto k-row k-row-center-items">
       <div className="col column-center">
-        <span>Loading...</span>
-        <span>To do: a loader with the logo</span>
+        <img src={logo} alt="logo" style={{ width: 200, height: 200 }} />
       </div>
     </div>
   );
